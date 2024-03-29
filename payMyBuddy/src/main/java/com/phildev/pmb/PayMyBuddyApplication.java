35966df520a37b0db7c0abda92196bdd88040b2a
Philip Mercan <philmercan@gmail.com>
2024-03-29 10:24:07 +0100

    adding deposit and money out form to handle money entering and leaving the app

--- payMyBuddy/src/main/java/com/phildev/pmb/PayMyBuddyApplication.java
+++ payMyBuddy/src/main/java/com/phildev/pmb/PayMyBuddyApplication.java
@@ -4,7 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PayMyBuddyApplication{
+public class PayMyBuddyApplication {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(PayMyBuddyApplication.class, args);
